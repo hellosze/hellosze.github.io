910b5b09239021cec6857e65ddbb2d25f05753c9
sze chan <sze@hello-sze.com>
2015-09-16 14:21:09 -0400

    Update ami.ads.gpt.js

--- ami.ads.gpt.js
+++ ami.ads.gpt.js
@@ -1,1 +1,14 @@
-console.log("Hello from ami.ads.gpt.js");
+var ami = ami || {};
+ami.ads = ami.ads || {};
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+
+ami.ads.gpt = (function(){
+  return {
+    bootstrap: function(){
+      var useSSL = 'https:' == document.location.protocol;
+      var src = (useSSL ? 'https:' : 'http:') +'//www.googletagservices.com/tag/js/gpt.js';
+      document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    }
+  }
+});
